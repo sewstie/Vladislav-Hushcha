e1488c0a5b4eea7b940718b306cbea25f35e4035
Vladislav Hushcha <vladislav.hushcha@gmail.com>
2024-03-09 18:02:40 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+      import { Application } from '/public/js/runtime.js';
+      const canvas = document.getElementById('canvas3d');
+      const app = new Application(canvas);
+      app.load('/src/3d-models/scene.splinecode');
