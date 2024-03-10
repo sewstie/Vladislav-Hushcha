363e136ebaff238aec7d0c1aa5af3d126ff8ec8f
Vladislav <vladislav.hushcha@gmail.com>
2024-03-10 14:59:55 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-      import { Application } from './public/js/runtime.js';
+      import { Application } from '/public/js/runtime.js';
       const canvas = document.getElementById('canvas3d');
       const app = new Application(canvas);
       app.load('./src/3d-models/scene.splinecode');
