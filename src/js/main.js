ea91266aa5c226251578b547beb8cdd0ff480e94
Vladislav Hushcha <vladislav.hushcha@gmail.com>
2024-05-08 00:05:48 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -99,4 +99,13 @@ gsap.to('.typewriter', {
   }
 })
 
-const customCursor = new dpkCursor({ ease: 0.03 });
+const blob = document.getElementById('blob');
+
+document.body.onpointermove = (event) => {
+  const {clientX, clientY} = event;
+
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+}, { duration: 3000, fill: 'forwards'});
+}
